16d095d4255e9e378eeffa48f4d1d996fa1a2bc0
stdenisj <stdenisjames@gmail.com>
2020-03-31 16:37:34 -0400

    created serializer for account and transactions

--- banking_app/serializers.py
+++ banking_app/serializers.py
@@ -1,1 +1,22 @@
 from rest_framework import serializers
+from .models import Account, Transaction
+
+class AccountSerializer(serializers.HyperlinkedModelSerializer):
+    transactions = serializers.HyperlinkedRelatedField(
+        view_name='transaction_detail',
+        many=True,
+        read_only=True
+    )
+    class Meta:
+        model = Account
+        fields = ('id', 'title', 'balance')
+
+class TransactionSerializer(serializers.HyperlinkedModelSerializer):
+    account = serializers.HyperlinkedRelatedField(
+        view_name='Account_detail',
+        many=False,
+        queryset=Account.objects.all()
+    )
+    class Meta:
+        model = Transaction
+        fields = ('id', 'description', 'amount', 'account', 'date')
