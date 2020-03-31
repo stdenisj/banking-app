e97e1dd5c5d4bdb6ee9465bd8f422606d77ac9e1
stdenisj <stdenisjames@gmail.com>
2020-03-31 17:12:10 -0400

    updated and created views

--- banking_app/views.py
+++ banking_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import AccountSerializer, TransactionSerializer
+from .models import Account, Transaction
 
-# Create your views here.
+class AccountView(viewsets.ModelViewSet):
+    queryset = Account.objects.all()
+    serializer_class = AccountSerializer
+
+class TransactionView(viewsets.ModelViewSet):
+    queryset = Transaction.objects.all()
+    serializer_class = TransactionSerializer
