b426b5cc35771d09c124d92204a8cea47367b982
stdenisj <stdenisjames@gmail.com>
2020-03-31 20:53:20 -0400

    fixed typo

--- banking_django/settings.py
+++ banking_django/settings.py
@@ -15,7 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-REACT_APP_DIR = os.path.join(BASE_DIR, 'Client')
+REACT_APP_DIR = os.path.join(BASE_DIR, 'client')
 
 STATICFILES_DIR = [
     os.path.join(REACT_APP_DIR, 'build', 'static')
