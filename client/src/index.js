33cbd85e80602148adbc601afb23f0fb92d31f31
stdenisj <stdenisjames@gmail.com>
2020-04-07 08:20:06 -0400

    removed strict mode

--- client/src/index.js
+++ client/src/index.js
@@ -7,9 +7,9 @@ import * as serviceWorker from './serviceWorker';
 
 
 ReactDOM.render(
-  <React.StrictMode>
+  <React>
     <App />
-  </React.StrictMode>,
+  </React>,
   document.getElementById('root')
 );
 
