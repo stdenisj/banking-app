eefa320c1355e1991ca0feb550424e8aa384e6f7
stdenisj <stdenisjames@gmail.com>
2020-04-07 07:53:15 -0400

    tying to push to heroku

--- banking_django/settings.py
+++ banking_django/settings.py
@@ -142,7 +142,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-# STATIC_URL = '/static/'
+STATIC_URL = '/static/'
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
