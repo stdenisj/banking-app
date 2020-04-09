27c3335fb32927513291c74f21506b5fc73eae19
stdenisj <stdenisjames@gmail.com>
2020-04-09 11:36:24 -0400

    added back react dir to test deploy

--- banking_django/settings.py
+++ banking_django/settings.py
@@ -16,9 +16,11 @@ import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-os.path.join(BASE_DIR, 'client', 'build', 'static')
+REACT_APP_DIR = os.path.join(BASE_DIR, 'client')
 
-STATICFILES_DIR = []
+STATICFILES_DIR = [
+    os.path.join(REACT_APP_DIR, 'build', 'static')
+]
 
 AUTH_USER_MODEL = 'accounts.AccountHolder'
 
