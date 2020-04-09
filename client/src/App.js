467f29604d90b4a85fe6b95971812c747d065e3e
stdenisj <stdenisjames@gmail.com>
2020-04-09 15:24:29 -0400

    commented out all components in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,31 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import AccountHolderView from './components/AccountHolderView'
-import LoginPage from './components/LoginPage'
+// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+// import AccountHolderView from './components/AccountHolderView'
+// import LoginPage from './components/LoginPage'
 
 export default class App extends Component {
-  state = {
-    token: {},
-    user: {}
-  }
+  // state = {
+  //   token: {},
+  //   user: {}
+  // }
 
-  getToken = (response) => {
-    this.setState({ token: response.token, user: response.user })
-  }
+  // getToken = (response) => {
+  //   this.setState({ token: response.token, user: response.user })
+  // }
 
   render() {
-    const AccountHolderViewComponent = () => (<AccountHolderView token={ this.state.token } user={ this.state.user }/>)
-    const LoginPageComponent = () => (<LoginPage  getToken={ this.getToken }/>)
+  //   const AccountHolderViewComponent = () => (<AccountHolderView token={ this.state.token } user={ this.state.user }/>)
+  //   const LoginPageComponent = () => (<LoginPage  getToken={ this.getToken }/>)
 
     return (
       <div className="App">
-        <Router>
+        <h1> hello world </h1>
+        {/* <Router>
           <Switch>
             <Route exact path = '/account' render = { AccountHolderViewComponent }/>
             <Route exact path = '/' render = { LoginPageComponent } />
           </Switch>
-        </Router>
+        </Router> */}
       </div>
     );
   }
